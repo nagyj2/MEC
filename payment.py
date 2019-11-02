e1135cdc17fe83a84b88be8de98279d24e71bc0d
Jason Nagy <nagyj2@mcmaster.ca>
2019-11-02 18:29:36 -0400

    Added comments

--- payment.py
+++ payment.py
@@ -1,15 +1,26 @@
 #!/usr/local/bin/python3
 from connection import Connection
 
+# Waits on user input and breaks it into an array.
+# Returns: A list of user inputted values where spaces act as delimiters.
 def getInput():
     command_string = input("> ")
     food_arr = [word.strip() for word in command_string.split()]
     return food_arr
     
+# Checks a ticket number to determine if it is valid to operate on.
+# Param: ticketno - The ticket number to check.
+# Param: db - A reference to a Connection class entity to operate on. It is
+#    assumed to have our defined Ticket table.
+# Returns: Boolean representing if it is ok for the ticket to be paid for.
+#   Returns false if the ticket number is invalid, the ticket has already been
+#   paid or there is an error executing the script.
 def verifyTicket(ticketno,db):
     ticketexist = "SELECT * FROM TICKET WHERE TICKETID = {}"
     ticketpaid = "SELECT STATUS FROM TICKET WHERE TICKETID = {}"
     
+    # Since the query could be interrupted by a database issue or the tables are
+    #   not set up properly, so surround the queries in a try statement.
     try:
         db.opencursor()
         if (not db.query(ticketexist.format(ticketno))):
@@ -23,9 +34,14 @@ def verifyTicket(ticketno,db):
         
     return True
     
+# Performs the database operation to update the status of a ticket.
+# Param: ticketno - The ticket number to update.
+# Param: db - A reference to the database Connection class entity to operate on.
 def payTicket(ticketno, db):
     updatestatus = "UPDATE TICKET SET STATUS = 1 WHERE TICKETID = {}"
 
+    # Like verifyTicket, an error could occur on the database side, so the
+    #   operation is surrounded in a try statement.
     try:
         db.opencursor()
         
@@ -37,13 +53,16 @@ def payTicket(ticketno, db):
         db.closecursor()
     
     
+# Function to validate a given ticket
 def verifyProcess(ticketno,db):
     valid = verifyTicket(ticketno,db)
     
     if (not valid):
-        raise Exception("Ticket is not valid")
+        # raise Exception("Ticket is not valid")
+        return False
         
     payTicket(ticketno, db)
+    return True
     
 
 def cli():
